3c87714bb7acca8e826f68299199209fa4552f7f
saddamhr <saddambubt65@gmail.com>
2021-01-03 11:09:54 +0600

    module 8 done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -32,7 +32,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
   const userExists = await User.findOne({ email })
 
-  if(userExists) {
+  if (userExists) {
     res.status(400)
     throw new Error('User already exists')
   }
@@ -40,10 +40,10 @@ const registerUser = asyncHandler(async (req, res) => {
   const user = await User.create({
     name,
     email,
-    password
+    password,
   })
 
-  if(user) {
+  if (user) {
     res.status(201).json({
       _id: user._id,
       name: user.name,
@@ -63,16 +63,44 @@ const registerUser = asyncHandler(async (req, res) => {
 const getUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id)
 
-  if(user) {
+  if (user) {
     res.json({
       _id: user._id,
       name: user.name,
       email: user.email,
       idAdmin: user.isAdmin,
     })
+  } else {
     res.status(404)
-    throw new Error('Invalid email or password')
+    throw new Error('User not found')
   }
 })
 
-export { authUser, registerUser, getUserProfile }
+// @desc    Update profile
+// @route   PUT /api/users/profile
+// @access  Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+
+  if (user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+    if (req.body.password) {
+      user.password = req.body.password
+    }
+    const updateUser = await user.save()
+
+    res.json({
+      _id: updateUser._id,
+      name: updateUser.name,
+      email: updateUser.email,
+      idAdmin: updateUser.isAdmin,
+      token: generateToken(updateUser._id),
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
+export { authUser, registerUser, getUserProfile, updateUserProfile }
