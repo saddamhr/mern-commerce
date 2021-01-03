8f00f25ecca9d1d5e29e706317a92ae53ec28431
saddamhr <saddambubt65@gmail.com>
2021-01-03 18:14:14 +0600

    order screen created

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,3 +1,4 @@
+import { response } from 'express'
 import asyncHandler from 'express-async-handler'
 import Order from '../models/orderModel.js'
 
@@ -35,4 +36,18 @@ const addOrderItems = asyncHandler(async (req, res) => {
   }
 })
 
-export { addOrderItems}
+// @desc    Get order by ID
+// @route   GET /api/orders/:id
+// @access  Private
+const getOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate('user', 'name email')
+
+  if(order) {
+    res.json(order)
+  } else{
+    res.status(404)
+    throw new Error('Order not found')
+  }
+})
+
+export { addOrderItems, getOrderById}
