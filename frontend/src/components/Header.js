d0a5215c209aea85e24fef43d5bfb8f30f3a6ea4
saddamhr <saddambubt65@gmail.com>
2021-01-04 13:26:18 +0600

    added paypal payments

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -31,16 +31,20 @@ const Header = () => {
               </LinkContainer>
               {userInfo ? (
                 <NavDropdown title={userInfo.name} id='username'>
-                  <LinkContainer to='profile'>
+                  <LinkContainer to='/profile'>
                     <NavDropdown.Item>Profile</NavDropdown.Item>
                   </LinkContainer>
-                  <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+                  <NavDropdown.Item onClick={logoutHandler}>
+                    Logout
+                  </NavDropdown.Item>
                 </NavDropdown>
-              ) : <LinkContainer to="/login">
-              <Nav.Link>
-                <i className="fas fa-user"></i> Sign In
-              </Nav.Link>
-            </LinkContainer>}
+              ) : (
+                <LinkContainer to='/login'>
+                  <Nav.Link>
+                    <i className='fas fa-user'></i> Sign In
+                  </Nav.Link>
+                </LinkContainer>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
